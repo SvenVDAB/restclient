06cff5ec0c63677098b4d95ee9ea216128f69098
SvenBouwen <sven.bouwen@vdabcampus.be>
2023-02-15 22:21:07 +0100

    Initial commit

---
+++ src/test/java/com/examplebe/vdab/restclient/RestclientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.examplebe.vdab.restclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestclientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
